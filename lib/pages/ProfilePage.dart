01546b63eec2961fd6656f464ae3a1bc6aee81fe
Nasywa Syafinka Widyamara <syafinkaforcolle@gmail.com>
2024-11-02 22:38:28 +0700

    laman regiser, login, dan profil

--- lib/pages/ProfilePage.dart
+++ lib/pages/ProfilePage.dart
@@ -15,20 +15,21 @@ class ProfilePage extends StatelessWidget {
             textStyle: const TextStyle(
               fontSize: 24.0,
               fontWeight: FontWeight.w900,
-              color: Color(0xFF191970), // Dark Blue Color
+              color: Color(0xFF191970),
             ),
           ),
         ),
         toolbarHeight: 89.0,
-        automaticallyImplyLeading: false, // Remove back button
+        automaticallyImplyLeading: false,
       ),
       backgroundColor: const Color(0xFFF9F9F9),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            // Profile Card
+            // Profile Card with full width
             Container(
+              width: double.infinity, // Set to full width
               padding: const EdgeInsets.all(16.0),
               decoration: BoxDecoration(
                 color: const Color(0xFF001C72),
@@ -36,182 +37,225 @@ class ProfilePage extends StatelessWidget {
               ),
               child: Column(
                 children: [
-                  // Centered Profile Picture and Text
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const CircleAvatar(
-                        radius: 100.0,
-                        backgroundColor: Colors.white,
-                        child: Icon(
-                          Icons.person, // Placeholder for profile picture
-                          size: 130.0,
-                          color: Color(0xFF001C72),
-                        ),
-                      ),
-                      const SizedBox(height: 16.0),
-                      const Text(
-                        "Hasan",
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                      const Text(
-                        "2241760069",
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          color: Colors.white,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
+                  const CircleAvatar(
+                    radius: 100.0,
+                    backgroundColor: Colors.white,
+                    child: Icon(
+                      Icons.person,
+                      size: 130.0,
+                      color: Color(0xFF001C72),
+                    ),
                   ),
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [],
+                  const SizedBox(height: 16.0),
+                  const Text(
+                    "Hasan",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const Text(
+                    "2241760069",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
                 ],
               ),
             ),
             const SizedBox(height: 20),
-            // Info Section (Centered)
+            // Info Section with Logout Button
             Container(
-              padding: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(12.0),
-                border: Border.all(color: Colors.grey),
+                border: Border.all(color: Colors.grey[300]!),
               ),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text(
-                        "Hasan Basri",
-                        style: TextStyle(fontSize: 16.0),
+                  _buildInfoRow("Hasan Basri"),
+                  _buildSeparator(),
+                  _buildInfoRow("2241760069"),
+                  _buildSeparator(),
+                  _buildInfoRow("Semester 5"),
+                  _buildSeparator(),
+                  _buildInfoRow("085876345109",
+                      trailing: const Icon(Icons.edit)),
+                  _buildSeparator(),
+                  // Logout Button
+                  GestureDetector(
+                    onTap: () {
+                      showModalBottomSheet(
+                        context: context,
+                        backgroundColor: Colors.transparent,
+                        isScrollControlled: true,
+                        builder: (BuildContext context) {
+                          return Center(
+                            child: Container(
+                              padding: const EdgeInsets.all(16.0),
+                              margin: const EdgeInsets.all(16.0),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20.0),
+                              ),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  const Text(
+                                    'Anda yakin ingin log out?',
+                                    style: TextStyle(fontSize: 18.0),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  const SizedBox(height: 20),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor:
+                                              const Color(0xFF001C72),
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(8.0),
+                                          ),
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 20.0),
+                                        ),
+                                        onPressed: () {
+                                          Navigator.of(context)
+                                              .pushReplacementNamed('/login');
+                                        },
+                                        child: const Text('YA'),
+                                      ),
+                                      ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor: Colors.grey,
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(8.0),
+                                          ),
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 20.0),
+                                        ),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: const Text('Tidak'),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text(
+                        "LogOut",
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ],
-                  ),
-                  const SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text(
-                        "2241760069",
-                        style: TextStyle(fontSize: 16.0),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text(
-                        "Semester 5",
-                        style: TextStyle(fontSize: 16.0),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text(
-                        "085876345109",
-                        style: TextStyle(fontSize: 16.0),
-                      ),
-                      Icon(Icons.edit),
-                    ],
+                    ),
                   ),
                 ],
               ),
             ),
             const Spacer(),
-            // LogOut Button
-            GestureDetector(
-              onTap: () {
-                showModalBottomSheet(
-                  context: context,
-                  backgroundColor: Colors.transparent,
-                  isScrollControlled: true,
-                  builder: (BuildContext context) {
-                    return Center(
-                      child: Container(
-                        padding: const EdgeInsets.all(16.0),
-                        margin: const EdgeInsets.all(16.0),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            const Text(
-                              'Anda yakin ingin log out?',
-                              style: TextStyle(fontSize: 18.0),
-                              textAlign: TextAlign.center,
-                            ),
-                            const SizedBox(height: 20),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: const Color(0xFF001C72),
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(8.0),
-                                    ),
-                                    padding: const EdgeInsets.symmetric(
-                                      horizontal: 20.0,
-                                    ),
-                                  ),
-                                  onPressed: () {
-                                    Navigator.of(context)
-                                        .pushReplacementNamed('/login');
-                                  },
-                                  child: const Text('YA'),
-                                ),
-                                ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: Colors.grey,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(8.0),
-                                    ),
-                                    padding: const EdgeInsets.symmetric(
-                                      horizontal: 20.0,
-                                    ),
-                                  ),
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: const Text('Tidak'),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                );
-              },
-              child: const Text(
-                "LogOut",
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 18.0,
-                ),
-              ),
-            ),
           ],
         ),
       ),
+      // Bottom navbar
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 5,
+        color: Colors.indigo[900],
+        child: SizedBox(
+          height: 70,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.home, color: Colors.white, size: 30),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.access_time,
+                    color: Colors.white, size: 30),
+                onPressed: () {},
+              ),
+              const SizedBox(width: 50),
+              IconButton(
+                icon: const Icon(Icons.mail, color: Colors.white, size: 30),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: const Icon(Icons.person, color: Colors.white, size: 30),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: Container(
+        width: 90,
+        height: 90,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.blueAccent,
+        ),
+        child: FloatingActionButton(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          onPressed: () {},
+          child: const Icon(
+            Icons.add,
+            size: 50,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+
+  Widget _buildInfoRow(String text, {Widget? trailing}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            text,
+            style: const TextStyle(fontSize: 16.0, color: Colors.black87),
+            textAlign: TextAlign.center,
+          ),
+          if (trailing != null) trailing,
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSeparator() {
+    return const Divider(
+      color: Colors.grey,
+      height: 1,
+      thickness: 0.5,
+      indent: 16,
+      endIndent: 16,
     );
   }
 }
--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'home_page.dart'; // menuju homepage
-import 'register.dart'; // page register
+import 'home_page.dart'; // homepage
 
-class LoginPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController nimController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
-  LoginPage({super.key});
+  // Role options and selected role
+  String selectedRole = 'Mahasiswa';
+  final List<String> roles = ['Mahasiswa', 'Admin', 'Dosen/Tendik'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, // Background to match the design
+      backgroundColor: Colors.white,
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
@@ -46,12 +52,42 @@ class LoginPage extends StatelessWidget {
               ),
               const SizedBox(height: 30),
 
-              // Text field for NIM with placeholder inside the box
+              // Role selection dropdown
               Container(
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(8.0),
                   border: Border.all(
-                    color: Colors.blue[900]!, // Blue border
+                    color: Colors.blue[900]!,
+                    width: 2.0,
+                  ),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: DropdownButton<String>(
+                  value: selectedRole,
+                  isExpanded: true,
+                  underline: SizedBox(),
+                  items: roles.map((String role) {
+                    return DropdownMenuItem<String>(
+                      value: role,
+                      child: Text(role, style: const TextStyle(fontSize: 18)),
+                    );
+                  }).toList(),
+                  onChanged: (String? newRole) {
+                    setState(() {
+                      selectedRole = newRole!;
+                    });
+                  },
+                  icon: const Icon(Icons.arrow_drop_down),
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              // Text field for NIM
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  border: Border.all(
+                    color: Colors.blue[900]!,
                     width: 2.0,
                   ),
                 ),
@@ -59,21 +95,20 @@ class LoginPage extends StatelessWidget {
                 child: TextField(
                   controller: nimController,
                   decoration: const InputDecoration(
-                    hintText: "NIM/NIP", // NIM placeholder
-                    border: InputBorder.none, // No extra border
+                    hintText: "NIM/NIP",
+                    border: InputBorder.none,
                   ),
                   style: const TextStyle(fontSize: 18),
                 ),
               ),
-
               const SizedBox(height: 20),
 
-              // Text field for Password with placeholder inside the box
+              // Text field for Password
               Container(
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(8.0),
                   border: Border.all(
-                    color: Colors.blue[900]!, // Blue border
+                    color: Colors.blue[900]!,
                     width: 2.0,
                   ),
                 ),
@@ -82,43 +117,15 @@ class LoginPage extends StatelessWidget {
                   controller: passwordController,
                   obscureText: true,
                   decoration: const InputDecoration(
-                    hintText: "Password", // Password placeholder
-                    border: InputBorder.none, // No extra border
+                    hintText: "Password",
+                    border: InputBorder.none,
                   ),
                   style: const TextStyle(fontSize: 18),
                 ),
               ),
-
               const SizedBox(height: 40),
 
-              // Add this after the password field container in the `Column` widget
-              const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("Belum punya akun? "),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              RegisterPage(), // Link to RegisterPage
-                        ),
-                      );
-                    },
-                    child: const Text(
-                      "Daftar Sekarang",
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-
-              // Login button
+              // Sign Up button
               ElevatedButton(
                 onPressed: () {
                   Navigator.push(
@@ -126,10 +133,10 @@ class LoginPage extends StatelessWidget {
                     MaterialPageRoute(
                       builder: (context) => HomePage(), // Navigate to HomePage
                     ),
-                  );
+                  ); // Add signup functionality here
                 },
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue[900], // Dark blue background
+                  backgroundColor: Colors.blue[900],
                   padding:
                       const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
                   shape: RoundedRectangleBorder(
@@ -137,7 +144,7 @@ class LoginPage extends StatelessWidget {
                   ),
                 ),
                 child: const Text(
-                  "Login",
+                  "Sign Up",
                   style: TextStyle(fontSize: 16, color: Colors.white),
                 ),
               ),
